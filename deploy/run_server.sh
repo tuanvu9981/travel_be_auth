51bf14b6db5bd57ed0255a0c154a185a90c43a3e
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2023-02-04 20:42:50 +0700

    ci: add deploy folder containing bash files (16)

--- deploy/run_server.sh
+++ deploy/run_server.sh
@@ -1,7 +1,9 @@
 # Pull code
 cd travel_be_auth
+npx kill-port 8000
+git pull origin master
 
 # Build and deploy
 npm install
 npm run build
-npm run serve 
+npm run serve > nestjs_server_running.log
