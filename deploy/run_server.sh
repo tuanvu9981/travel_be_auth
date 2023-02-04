cc106d2c835aa77e3b4ffe41f049a00666fa4428
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2023-02-05 00:29:31 +0700

    ci: add deploy folder containing bash files (25)

--- deploy/run_server.sh
+++ deploy/run_server.sh
@@ -7,8 +7,8 @@ git pull origin master
 npm install
 npm run build
 echo "Build: done"
-npm run serve
-timeout 180s echo "Running ..."
+timeout 200s npm run serve
+echo "Running ..."
 # timeout 180s npm run serve > nestjs_server_running.log || FAILED=true
 # grep -e 'listening on \*:8000' nestjs_server_running.log  
 # if [ "$FAILED" == "true" ]; then exit 0; fi
