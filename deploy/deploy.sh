9031fb5db73f819a6bda7027705f0ac60fdc9a62
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-09-20 09:30:14 +0700

    ci: change env config (3)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "Starting deploying to $EC2_IP_ADDRESS ... "
+
+# Get sensitive data from ENV_DATA in CICD storage, copy to .env file in repo.
+ssh ubuntu@$EC2_IP_ADDRESS 'cd travel_be_auth && rm -f .env && cat $ENV_DATA >> .env'
+
 ssh ubuntu@$EC2_IP_ADDRESS 'bash' < ./deploy/run_server.sh
