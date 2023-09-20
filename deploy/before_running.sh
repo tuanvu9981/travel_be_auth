7daf066d07d8a81028c4d2b4e8fee6d4b114c537
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-09-20 09:54:44 +0700

    ci: change env config (5)

--- deploy/before_running.sh
+++
@@ -1,5 +0,0 @@
-pwd
-cd travel_be_auth
-rm -f .env
-cat $ENV_DATA >> .env
-exit 0
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 echo "Starting deploying to $EC2_IP_ADDRESS ... "
-
-# Get sensitive data from ENV_DATA in CICD storage, copy to .env file in repo.
-ssh ubuntu@$EC2_IP_ADDRESS 'bash' < ./deploy/before_running.sh
-
 ssh ubuntu@$EC2_IP_ADDRESS 'bash' < ./deploy/run_server.sh
