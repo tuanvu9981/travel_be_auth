ef973861b1df5fcea75557a6dc3b3a8becbf2d9a
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-09-20 10:38:52 +0700

    ci: change env config (9)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting deploying to $EC2_IP_ADDRESS ... "
-export $ENV_DATA
+ssh ubuntu@$EC2_IP_ADDRESS "cd travel_be_auth && rm -f .env && cat $ENV_DATA >> .env"
 ssh ubuntu@$EC2_IP_ADDRESS 'bash' < ./deploy/run_server.sh
