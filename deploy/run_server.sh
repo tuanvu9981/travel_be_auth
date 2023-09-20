10873a25aa926e78168e58929fed003e5f897483
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-09-20 09:17:51 +0700

    ci: change env config (2)

--- deploy/run_server.sh
+++ deploy/run_server.sh
@@ -1,9 +1,5 @@
 # Pull code & remove old application taking port 8000
 cd travel_be_auth
-# Get sensitive data from ENV_DATA in CICD storage, copy to .env file in repo.
-pwd
-rm -f .env
-cat $ENV_DATA >> .env
 npx kill-port 8000
 git pull origin master
 
