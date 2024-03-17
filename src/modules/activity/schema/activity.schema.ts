4cfb8fe813e6895ba4802e809f9fa1e5b736a034
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2024-03-17 17:34:20 +0900

    feat: add data.json and change schema to apply multiple languages (2)

--- src/modules/activity/schema/activity.schema.ts
+++ src/modules/activity/schema/activity.schema.ts
@@ -1,27 +1,40 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
+import { DefaultMultipleLangString, MultipleLangString } from "src/common/constant/language.const";
 
 export type ActivityDocument = Activity & Document;
 
+enum ACTIVITY_TYPE {
+    SIGH_SEEING = "sigh_seeing",
+    CULTURE_EXPERIENCE = "culture_experience",
+    FOOD_TOUR = "food_tour",
+    LOCAL_MARKET = "local_market",
+    RELIGIOUS_PLACE = "religious_place"
+}
+
 @Schema({ versionKey: false })
 export class Activity {
+
+    @Prop({ type: MultipleLangString, default: DefaultMultipleLangString })
+    price: MultipleLangString;
+
+    @Prop({ type: MultipleLangString, default: DefaultMultipleLangString })
+    name: MultipleLangString;
+
+    @Prop({
+        default: [],
+        type: [{ type: String, enum: ACTIVITY_TYPE }]
+    })
+    type: string[];
+
     @Prop()
     imageUrl: string;
 
     @Prop()
-    name: string;
-
-    @Prop()
-    type: string;
-
-    @Prop()
-    startTimes: string[];
+    businessTime: string;
 
     @Prop()
     rating: number;
-
-    @Prop()
-    price: number;
 }
 
 export const ActivitySchema = SchemaFactory.createForClass(Activity);
--- src/modules/hotel/schema/hotel.schema.ts
+++ src/modules/hotel/schema/hotel.schema.ts
@@ -1,24 +1,37 @@
 import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 import { Floor } from "./room.entity";
+import { DefaultMultipleLangString, MultipleLangString } from "src/common/constant/language.const";
 
 export type HotelDocument = Hotel & Document;
 
 @Schema({ versionKey: false })
 export class Hotel {
+
+    @Prop({
+        required: true,
+        type: MultipleLangString,
+        default: DefaultMultipleLangString
+    })
+    name: MultipleLangString;
+
+    @Prop({
+        required: true,
+        type: MultipleLangString,
+        default: DefaultMultipleLangString
+    })
+    address: MultipleLangString;
+
+    @Prop({ type: MultipleLangString, default: DefaultMultipleLangString })
+    price: MultipleLangString;
+
     @Prop({ required: true })
     imageUrl: string;
 
-    @Prop({ required: true })
-    name: string;
-
-    @Prop({ required: true })
-    address: string;
-
-    @Prop()
-    price: number;
-
-    @Prop({ default: [] })
+    @Prop({
+        default: [],
+        type: [{ type: Floor }]
+    })
     floors: Floor[];
 }
 
